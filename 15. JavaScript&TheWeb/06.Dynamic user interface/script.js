95bb456d4f0d3b1de64e761e420755349e691a96
Mélanie Thomas <melanie.thomas.design@gmail.com>
2022-04-27 11:32:19 +0200

    update 2022.04.27 11:30

--- 15. JavaScript&TheWeb/06.Dynamic user interface/script.js
+++ 15. JavaScript&TheWeb/06.Dynamic user interface/script.js
@@ -1,34 +1,51 @@
-const myButton = document.getElementById("myButton");
+const addButton = document.getElementById("addButton");
+const removeButton = document.getElementById("removeButton");
+const clearButton = document.getElementById("ClearButton");
 const myText = document.getElementById("myText");
 const content = document.getElementById("content");
-const remove1stParaButton = document.getElementById("remove1stParaButton");
-const removeLastParaButton = document.getElementById("removeLastParaButton");
-const main = document.getElementById("main");
+
 
 function addParagraph(){
     const newParagraph = document.createElement("p");
     newParagraph.innerText = myText.value;
-    newParagraph.className = "beautiful";
+    newParagraph.className = "newItem";
     
     content.appendChild(newParagraph);
 }
 
-function removeFirstParagraph(){
-    const paragraphs = document.getElementsByClassName("beautiful");
-    if(paragraphs.length > 0){
-        content.removeChild(paragraphs[0]);
-    }
-
-}
+// // pour chaque nouveau paragraphe écrit je veux enregistrer dans le storage un key qui correspond à l'index et une value qui est l'innerText à cet index
+// function storeParagraph(){
+//     for (newParagraph in content){
+//         localStorage.setItem(String(content.indexOf(newParagraph), String(newParagraph.innerText)); 
+//     }
+// }
 
 function removeLastParagraph(){
-    const paragraphs = document.getElementsByClassName("beautiful");
+    const paragraphs = document.getElementsByClassName("newItem");
     if(paragraphs.length > 0){
         content.removeChild(paragraphs[paragraphs.length - 1])
     }
+
 }
 
-myButton.addEventListener("click", addParagraph);
-remove1stParaButton.addEventListener("click", removeFirstParagraph);
-removeLastParaButton.addEventListener("click", removeLastParagraph);
+function clearParagraph(){
+    const paragraphs = document.getElementsByClassName("newItem");
+    for (i in paragraphs) {
+        if(paragraphs.length > 0){
+            paragraphs.removeChild(i)
+    }
+    
+    }
+}
 
+localStorage.clear();
+addButton.addEventListener("click", addParagraph);
+//addButton.addEventListener("click", storeParagraph);
+removeButton.addEventListener("click", removeLastParagraph);
+clearButton.addEventListener("click", clearParagraph);
+
+// Exercice :
+// -les textes devront apparaître dans le block gris en haut de page
+// -button Add item = ajouter un nouveau text dans le block + dans le storage 
+// -button Remove last item = enlever le dernier text dans le block + dans le storage + pas d'erreur si on supprime du texte alors qu'il n'y en a plus 
+// -button Clear all = enlever tous les textes à l'écran et dans le storage +pas d'erreur si on supprime du texte alors qu'il n'y en a plus 
